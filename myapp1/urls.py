0e622d8b42e2fb9c35df720a727a6a076bde9b67
vpnvsk <vlad.pnevsk@gmail.com>
2023-02-23 19:42:54 +0100

    added brand filter

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.ProductView.as_view()),
+app_name = 'myapp1'
+
+urlpatterns = [path('', views.ProductView.as_view(), name='index'),
                path('<int:pk>/', views.CertainProductView.as_view())]
--- myapp1/views.py
+++ myapp1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from .models import Products, Shoe_size
+from .models import Products, Shoe_size, Brand
 
 
 
@@ -12,8 +12,21 @@ def indexPage(request):
 class ProductView(View):
     #main page
     def get(self, request):
+
         product_view = Products.objects.all()
-        return render(request, 'index.html' , {'product_list':product_view})
+        brands = Brand.objects.all()
+        brand_id = request.GET.get('brand', 0)
+
+        if brand_id:
+            product_view = product_view.filter(brand=brand_id)
+
+        return render(request, 'index.html' , {
+            'product_list':product_view,
+            'brands': brands,
+            'brand_id':int(brand_id)
+            })
+    
+
 
 class CertainProductView(View):
     #certain product
