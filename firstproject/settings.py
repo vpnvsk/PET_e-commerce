618148462a354464292de4fad684a64015f9f8e0
vpnvsk <vlad.pnevsk@gmail.com>
2023-03-03 23:04:37 +0100

    moving to postgreSQL

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PET_1',
+        'USER': 'postgres',
+        'PASSWORD': '4566',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
