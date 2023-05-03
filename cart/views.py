1fe181b5b5275c0f4b75f09d2722e735aab07d90
vpnvsk <vlad.pnevsk@gmail.com>
2023-05-03 20:48:32 +0200

    cart views imports refactoring

--- cart/views.py
+++ cart/views.py
@@ -1,22 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
-from userAuthentication.models import Profile
-from .models import Order,OrderItem
-from productsContent.models import Products, Brand,ProductSize,Size
-from django.contrib import messages
-from .extra import generate_ref_code
-from django.urls import reverse
-from django.utils import timezone
-from django.db.models import F
-from productsContent.forms import SizeForm
-from django.http import JsonResponse
-from productsContent.views import CertainProductView
 from django.views.generic import View
+
+from .models import Order
+from productsContent.models import Products, Brand
 from .cartLogic import add_to_cart, remove_item
-
-
-import datetime
 
 
 def get_checkout(request):
@@ -54,27 +42,27 @@ class AddToCartView(View):
         return redirect("productsContent:certain_product",pk=item.id)
 
                  
-@login_required
-def updete_transaction_records(request,order_id):
+# @login_required
+# def updete_transaction_records(request,order_id):
 
-    order_to_purchase = Order.objects.filter(pk=order_id).first()
+#     order_to_purchase = Order.objects.filter(pk=order_id).first()
 
-    order_to_purchase.is_ordered = True
-    order_to_purchase.date_ordered = datetime.datetime.now()
-    order_to_purchase.save()
+#     order_to_purchase.is_ordered = True
+#     order_to_purchase.date_ordered = datetime.datetime.now()
+#     order_to_purchase.save()
 
-    order_items = order_to_purchase.items.all()
-    order_items.update(is_ordered=True, date_ordered = datetime.datetime.now())
+#     order_items = order_to_purchase.items.all()
+#     order_items.update(is_ordered=True, date_ordered = datetime.datetime.now())
 
-    user_profile = get_object_or_404(Profile, user = request.user)
+#     user_profile = get_object_or_404(Profile, user = request.user)
 
-    order_products = [item.product for item in order_items]
-    user_profile.prod.add(*order_products)
-    user_profile.save()
+#     order_products = [item.product for item in order_items]
+#     user_profile.prod.add(*order_products)
+#     user_profile.save()
 
-    messages.info(request, 'Thank you')
+#     messages.info(request, 'Thank you')
 
-    return redirect('/')
+#     return redirect('/')
 
 
 
